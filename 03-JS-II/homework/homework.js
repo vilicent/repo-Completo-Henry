930aa8cceebb5233ced0a8c29288d15cf9488ff6
vilicent <nicanormquiroga@gmail.com>
2022-08-11 18:17:38 -0300

    a

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -5,12 +5,22 @@ function obtenerMayor(x, y) {
   // Devuelve el número más grande
   // Si son iguales, devuelve cualquiera de los dos
   // Tu código:
+    if (x > y) {
+      return x 
+    } else {
+      return y
+    }
 }
 
 function mayoriaDeEdad(edad) {
   //Determinar si la persona según su edad puede ingresar a un evento.
   //Si tiene 18 años ó más, devolver --> "Allowed"
   //Si es menor, devolver --> "Not allowed"
+  if (edad > 17) {
+    return "Allowed"
+  } else {
+    return "Not allowed"
+  }
 }
   
 function conection(status) {
@@ -19,6 +29,12 @@ function conection(status) {
   //Cuando el estado es igual a 2, el usuario está "Away"
   //De lo contrario, presumimos que el usuario está "Offline"
   //Devolver el estado de conexión de usuario en cada uno de los casos.
+  if (status === 1) {
+    return "Online"
+  }else if (status === 2) {
+    return "Away"
+  }
+    return "Offline"
 }
 
 function saludo(idioma) {
@@ -28,6 +44,16 @@ function saludo(idioma) {
   // Si "idioma" es "ingles", devuelve "Hello!"
   // Si "idioma" no es ninguno de los anteiores o es `undefined` devuelve "Hola!"
   // Tu código:
+  if (idioma == "aleman") {
+    return "Guten Tag!"
+  } else if (idioma = "mandarin") {
+    return "Ni Hao!"
+  }else if (idioma = "ingles") {
+    return "Hello!"
+  }else if (idioma = undefined) {
+    return "Hola!"
+  }
+
 }
 
 function colors(color) {
@@ -38,6 +64,27 @@ function colors(color) {
   //En caso que el color recibido sea "orange", devuleve --> "This is orange"
   //Caso default: devuelve --> "Color not found"
   //Usar el statement Switch.
+    switch (color) {
+      case "blue":
+         return "This is " + color 
+        break;
+
+        case "red":
+          return "This is " + color
+          break;
+
+          case "green":
+            return "This is " + color
+          break;
+
+          case "orange":
+            return "This is " + color
+          break;
+    
+      default:
+        
+        break;
+    }
 }
 
 function esDiezOCinco(numero) {
